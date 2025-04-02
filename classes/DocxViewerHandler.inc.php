10b8f67b1195d25d4f37cb92c7bbd03abfaabf81
Cristianc72004 <benalcazarc7@gmail.com>
2025-04-02 00:11:12 -0500

    5 verison

--- classes/DocxViewerHandler.inc.php
+++ classes/DocxViewerHandler.inc.php
@@ -3,17 +3,33 @@
 import('classes.handler.Handler');
 import('lib.pkp.classes.file.PrivateFileManager');
 
+use APP\facades\Repo;
+use PKP\submissionFile\SubmissionFile;
+
 class DocxViewerHandler extends Handler {
     function authorize($request, &$args, $roleAssignments) {
         import('lib.pkp.classes.security.authorization.WorkflowStageAccessPolicy');
-        $this->addPolicy(new WorkflowStageAccessPolicy($request, $args, $roleAssignments, 'submissionId', (int) $request->getUserVar('stageId')));
+        $this->addPolicy(new WorkflowStageAccessPolicy(
+            $request,
+            $args,
+            $roleAssignments,
+            'submissionId',
+            (int) $request->getUserVar('stageId')
+        ));
         return parent::authorize($request, $args, $roleAssignments);
     }
 
     function view($args, $request) {
         $submissionFileId = (int) $request->getUserVar('submissionFileId');
-        $submissionFile = Services::get('submissionFile')->get($submissionFileId);
-        if (!$submissionFile) die('Archivo no encontrado');
+        $submissionId = (int) $request->getUserVar('submissionId');
+
+        // Usar el repositorio moderno para obtener el archivo
+        $submissionFile = Repo::submissionFile()->get($submissionFileId);
+
+        // Validar que el archivo existe y pertenece al envío
+        if (!$submissionFile || $submissionFile->getData('submissionId') !== $submissionId) {
+            die('Archivo no encontrado o no autorizado');
+        }
 
         $fileManager = new PrivateFileManager();
         $filePath = $fileManager->getBasePath() . DIRECTORY_SEPARATOR . $submissionFile->getData('path');
@@ -22,6 +38,7 @@ class DocxViewerHandler extends Handler {
             die('Archivo no disponible en el servidor');
         }
 
+        // Enviar archivo .docx al navegador
         header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
         header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
         header('Content-Length: ' . filesize($filePath));
